5d73db9bb3e954eefdfe4d2994e8cbdb89201baa
Arne De Smedt <arnedesmdt@gmail.com>
2021-02-19 09:09:32 +0100

    add string utils

--- src/StringUtil.php
+++ src/StringUtil.php
@@ -5,17 +5,35 @@ declare(strict_types=1);
 namespace ADS\Util;
 
 use ADS\Util\Exception\StringUtilException;
+use RuntimeException;
 
+use function array_filter;
+use function array_map;
+use function array_search;
+use function array_slice;
+use function explode;
+use function floatval;
+use function implode;
+use function intval;
+use function is_numeric;
 use function lcfirst;
 use function preg_replace;
+use function reset;
+use function sort;
 use function sprintf;
 use function str_replace;
+use function strpos;
 use function strtolower;
 use function trim;
 use function ucwords;
 
 final class StringUtil
 {
+    public const ENTITY_PREFIX_NAMES = [
+        'Entity',
+        'Model',
+    ];
+
     public static function camelize(string $string, string $delimiter = '_', bool $pascal = false): string
     {
         $result = str_replace($delimiter, '', ucwords($string, $delimiter));
@@ -45,5 +63,118 @@ final class StringUtil
         $trim = preg_replace('/[^A-Za-z0-9-]+/', '-', $slug);
 
         return strtolower(trim($trim, '-'));
+    }
+
+    /**
+     * @return mixed
+     */
+    public static function castFromString(string $string)
+    {
+        switch (true) {
+            case $string === 'false':
+                return false;
+
+            case $string === 'true':
+                return true;
+
+            case is_numeric($string) && strpos($string, '.') !== false:
+                return floatval($string);
+
+            case is_numeric($string):
+                return intval($string);
+
+            default:
+                return $string;
+        }
+    }
+
+    /**
+     * @param array|string[] $prefixes
+     */
+    public static function entityNamespaceFromClassName(
+        string $className,
+        array $prefixes = self::ENTITY_PREFIX_NAMES
+    ): string {
+        $positions = self::positionsOfPrefixes($className, $prefixes);
+        $resourceNameParts = explode('\\', $className);
+
+        if (! empty($positions)) {
+            $position = reset($positions);
+            $namespaceParts = array_slice($resourceNameParts, 0, $position + 1);
+
+            if (empty($namespaceParts)) {
+                return '\\';
+            }
+
+            return implode('\\', $namespaceParts);
+        }
+
+        throw new RuntimeException(
+            sprintf(
+                'Entity or Model name not found for class \'%s\'.',
+                $className
+            )
+        );
+    }
+
+    /**
+     * @param array|string[] $prefixes
+     */
+    public static function entityNameFromClassName(
+        string $className,
+        array $prefixes = self::ENTITY_PREFIX_NAMES
+    ): string {
+        // First try to find the short name by the class
+        // Classes like *\Entity\Bank\* or *\Model\Bank\* will result in short name: Bank.
+        $positions = self::positionsOfPrefixes($className, $prefixes);
+        $resourceNameParts = explode('\\', $className);
+
+        if (! empty($positions)) {
+            foreach ($positions as $position) {
+                $shortName = $resourceNameParts[$position + 1] ?? null;
+
+                if ($shortName) {
+                    return $shortName;
+                }
+            }
+        }
+
+        throw new RuntimeException(
+            sprintf(
+                'Entity or Model name not found for class \'%s\'.',
+                $className
+            )
+        );
+    }
+
+    /**
+     * @param array<string> $prefixes
+     *
+     * @return array<int>
+     */
+    private static function positionsOfPrefixes(
+        string $className,
+        array $prefixes = self::ENTITY_PREFIX_NAMES
+    ): array {
+        $resourceNameParts = explode('\\', $className);
+
+        $positions = array_filter(
+            array_map(
+                static function (string $prefixOfResourceName) use ($resourceNameParts) {
+                    $position = array_search($prefixOfResourceName, $resourceNameParts);
+
+                    if ($position === false) {
+                        return null;
+                    }
+
+                    return $position;
+                },
+                $prefixes
+            )
+        );
+
+        sort($positions);
+
+        return $positions;
     }
 }
