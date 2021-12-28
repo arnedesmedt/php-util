d07d3eb5ca15cd892d97150eb9a34215c3607503
Arne De Smedt <arnedesmdt@gmail.com>
2021-12-28 18:08:48 +0100

    rector to php80

--- rector.php
+++ rector.php
@@ -12,7 +12,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters->set(Option::PATHS, [__DIR__ . '/src']);
 
     // Define what rule sets will be applied
-    $containerConfigurator->import(LevelSetList::UP_TO_PHP_81);
+    $containerConfigurator->import(LevelSetList::UP_TO_PHP_80);
 
     // get services (needed for register a single rule)
     // $services = $containerConfigurator->services();
