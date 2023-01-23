f4e6ec48794747d6a4557e7c47f6272cfcb1b323
Arne De Smedt <arnedesmdt@gmail.com>
2023-01-23 19:56:26 +0100

    Remove rector specific config

--- rector.php
+++ rector.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
-use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
 use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
@@ -12,12 +11,4 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([__DIR__ . '/src', __DIR__ . '/tests']);
     $rectorConfig->phpVersion(PhpVersion::PHP_81);
     $rectorConfig->import(LevelSetList::UP_TO_PHP_81);
-
-    $rectorConfig->skip(
-        [
-            ReadOnlyPropertyRector::class => [
-                __DIR__ . '/tests/Object',
-            ],
-        ],
-    );
 };
