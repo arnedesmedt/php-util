5146e5996b54342354f78ba0168394820b04db30
Arne De Smedt <arnedesmdt@gmail.com>
2023-12-11 18:45:14 +0100

    Create symfony bundle

---
+++ src/ADSUtilBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ADS\Util;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class ADSUtilBundle extends AbstractBundle
+{
+}
---
+++ src/DependencyInjection/ADSUtilExtension.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ADS\Util\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class ADSUtilExtension extends Extension
+{
+    /** @param array<mixed> $configs */
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../Resources/config'),
+        );
+
+        $loader->load('php_util.yaml');
+    }
+}
